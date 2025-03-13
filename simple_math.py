a39c93e8282d0954ac8a1b6ab6e00933a2a5179d
Michael <mznamenshchykov@gmail.com>
2025-03-13 22:38:46 +0100

    Initial commit - added project files

---
+++ simple_math.py
@@ -0,0 +1,10 @@
+class SimpleMath:
+    """A class with basic mathematical operations."""
+
+    def square(self, x):
+        """Returns the square of a number."""
+        return x * x
+
+    def cube(self, x):
+        """Returns the cube of a number."""
+        return x * x * x
